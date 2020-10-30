81cbd52f066595cb2177b4c12144128da52a168e
Asim-Tahir <AsimTahir@protonmail.com>
2020-10-31 00:16:29 +0300

    Init: Home view init. Just include Search comp.

---
+++ app/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+<Search />
+</template>
+
+<script lang="ts">
+import {
+  defineComponent
+} from "vue";
+
+import Search from "@/components/Search.vue"
+
+export default defineComponent({
+  name: "Home",
+  components: {
+    Search
+  }
+});
+</script>
