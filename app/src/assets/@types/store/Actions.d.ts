7bdad3b949f08df951a42ea2b8842533c495fa81
Asim-Tahir <AsimTahir@protonmail.com>
2020-11-01 00:34:19 +0300

    Add state to all actions for fetch options. Add dispatch method to AugmentedActionContext

--- app/src/assets/@types/store/Actions.d.ts
+++ app/src/assets/@types/store/Actions.d.ts
@@ -1,21 +1,27 @@
-import {ActionContext} from 'vuex'
-import {State} from '../store/State'
-import {Mutations} from '../store/Mutations'
+import { ActionContext } from 'vuex'
+
+import { State } from './State'
+import { Mutations } from '.Mutations'
+import { Getters } from './Getters'
 
 type AugmentedActionContext = {
   commit<K extends keyof Mutations>(
     key: K,
     payload: Parameters<Mutations[K]>[1]
-  ): ReturnType<Mutations[K]>,
-  getters<K extends keyof Mutations>(
-    key: K,
-    payload: Parameters<Mutations[K]>[2]
   ): ReturnType<Mutations[K]>
+  dispatch<K extends keyof Actions>(
+    key: K,
+    payload: Parameters<Actions[K]>[1]
+  ): ReturnType<Actions[K]>
+  // getters<K extends keyof Getters>(
+  //   key: K,
+  // ): ReturnType<Getters[K]> // TODO: 
+  // state: State, // TODO: 
 } & Omit<ActionContext<State, State>, 'commit'>
 
 export interface Actions {
-  GET_API_REDDITOR_SUBMISSIONS({ commit, getters }: AugmentedActionContext): void
-  GET_API_REDDITOR_COMMENTS({ commit, getters }: AugmentedActionContext): void
-  GET_API_USERNAME_AVAILABLE({commit, getters}: AugmentedActionContext): void
-  GET_API_REDDITOR_DATA({commit, getters}: AugmentedActionContext): void
+  GET_API_REDDITOR_SUBMISSIONS({ commit, getters, state }: AugmentedActionContext): void
+  GET_API_REDDITOR_COMMENTS({ commit, getters, state }: AugmentedActionContext): void
+  GET_API_USERNAME_AVAILABLE({ commit, getters, state }: AugmentedActionContext): void
+  GET_API_REDDITOR_DATA({ commit, getters, state }: AugmentedActionContext): void
 }
