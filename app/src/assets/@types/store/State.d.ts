725137a3a1886412e0692d3c63f70c6edbdc1db8
Asim-Tahir <AsimTahir@protonmail.com>
2020-11-01 00:35:23 +0300

    Some shorthand and Added Fetch API Options

--- app/src/assets/@types/store/State.d.ts
+++ app/src/assets/@types/store/State.d.ts
@@ -1,43 +1,22 @@
 import {Submission} from '../Submission'
 import {Comment} from '../Comment'
+import { Redditor } from '../Redditor'
 
 export interface State {
   Search: {
     UsernameAvailable: Boolean,
     Suggestion: Array<String>
   },
-  Redditor: {
-    Username: string
-    ProfilePhoto: {
-      URL: string
-      Width: number
-      Height: number
-    }
-    Banner: {
-      URL: string
-      Width: number
-      Height: number
-    }
-    IsBanned: boolean
-    PublicDescription: string
-    Subscriber: number
-    CreatedDate: {
-      Raw: Date
-      UTCDate: Date
-    }
-    LastSubmissionActivityDate: Date
-    LastCommentActivityDate: Date
-    Submissions: Array<Submission>
-    Comments: Array<Comment>
-  },
+  Redditor: Redditor,
   API: {
     Base:{
-      PushShift: String,
+      PushShift: String
       Reddit: String
     },
     Filter: {
-      Submission: String,
-      Comment: String,
-    }
+      Submission: String
+      Comment: String
+    },
+    Options: RequestInit
   }
 }
