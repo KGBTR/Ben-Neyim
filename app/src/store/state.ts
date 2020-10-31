722383c72a91f9ee1992855b81ec277a82ad8bcc
Asim-Tahir <AsimTahir@protonmail.com>
2020-11-01 00:50:34 +0300

    State type assign to state object Class type init removed. Set default data Add Option object for fetch option

--- app/src/store/state.ts
+++ app/src/store/state.ts
@@ -1,26 +1,31 @@
 import {Comment} from '@/assets/@types/Comment'
+import { State } from '@/assets/@types/store/State'
 import {Submission} from '@/assets/@types/Submission'
 
-export const state = {
+export const state: State = {
   Search: {
-    UsernameAvailable: Boolean(),
+    UsernameAvailable: false,
     Suggestion: Array<string>()
   },
   Redditor: {
-    Username: String(),
+    Username: '',
+    UsernameLength: {
+      Min: 3,
+      Max: 20,
+    },
     ProfilePhoto: {
-      URL: String(),
-      Width: Number(),
-      Height: Number(),
+      URL: '',
+      Width: 48,
+      Height: 48,
     },
     Banner: {
-      URL: String(),
-      Width: Number(),
-      Height: Number(),
+      URL: '',
+      Width: 0,
+      Height: 0,
     },
-    IsBanned: Boolean(),
-    PublicDescription: String(),
-    Subscriber: Number(),
+    IsBanned: false,
+    PublicDescription: '',
+    Subscriber: 0,
     CreatedDate: {
       Raw: new Date(),
       UTCDate: new Date(),
@@ -38,6 +43,14 @@ export const state = {
     Filter: {
       Submission: 'title,selftext,full_link,score,thumbnail,thumbnail_height,thumbnail_width,link_flair_background_color,link_flair_text,link_flair_text_color,created_utc,over_18,spoiler',
       Comment: 'body,permalink,created_utc,parent_id,retrieved_on,score,is_submitter',
+    },
+    Options: {
+      method: 'GET',
+      mode: 'no-cors',
+      cache: 'force-cache',
+      headers: {
+        'Content-Type': 'application/json',
+      },
     }
   },
 }
