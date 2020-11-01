b539d2c138d06b4f04bd78c9210cdd6b29d3d727
Asim-Tahir <AsimTahir@protonmail.com>
2020-11-01 23:53:17 +0300

    SET: font-family set in root App component

--- app/src/App.vue
+++ app/src/App.vue
@@ -3,5 +3,18 @@
 </template>
 
 <style lang="scss">
-
+body {
+  font-family: 'Rubik', sans-serif;
+  font-size: 14pt;
+}
 </style>
+
+<script lang="ts">
+import {
+  defineComponent
+} from 'vue'
+
+export default defineComponent({
+  name: 'App'
+})
+</script>
