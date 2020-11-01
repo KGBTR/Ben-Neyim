dceb03047b818476395e8c5cdf9cf0a924ce95c5
Asim-Tahir <AsimTahir@protonmail.com>
2020-11-01 23:54:13 +0300

    TRY: Sentry Vue entegration try. Its not compatiable with Vue 3. Its could be fixed

--- app/src/main.ts
+++ app/src/main.ts
@@ -4,6 +4,27 @@ import "./registerServiceWorker";
 import router from "./router";
 import {store} from "./store";
 
+// import Vue from "vue";
+import * as Sentry from "@sentry/browser";
+import { Vue as VueIntegration } from "@sentry/integrations";
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: process.env['SENTRY_DSN'],
+  integrations: [
+    new VueIntegration({
+      // Vue: Vue, // FIXME:  Vue 3 entgration failed :(
+      tracing: true,
+      logErrors: true,
+    }),
+    new Integrations.BrowserTracing(),
+  ],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
+
 createApp(App)
   .use(store)
   .use(router)
