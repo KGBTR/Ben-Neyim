9128c6743a2e25549e85dbd34d72c5b197266a62
Asim-Tahir <AsimTahir@protonmail.com>
2020-11-01 23:51:37 +0300

    INIT: User sharable link view component init

---
+++ app/src/views/User.vue
@@ -0,0 +1,20 @@
+<template>
+<div>{{$route.params.username}}</div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
+
+<script lang="ts">
+import {
+  defineComponent
+} from 'vue'
+
+export default defineComponent({
+  setup() {
+
+    return {}
+  }
+})
+</script>
