f358b4ca41b23d55f4ce886a89753f693ecc8957
Asim-Tahir <AsimTahir@protonmail.com>
2020-11-02 21:28:16 +0300

    Sentry integration comment out

--- app/src/main.ts
+++ app/src/main.ts
@@ -5,25 +5,25 @@ import router from "./router";
 import {store} from "./store";
 
 // import Vue from "vue";
-import * as Sentry from "@sentry/browser";
-import { Vue as VueIntegration } from "@sentry/integrations";
-import { Integrations } from "@sentry/tracing";
+// import * as Sentry from "@sentry/browser";
+// import { Vue as VueIntegration } from "@sentry/integrations";
+// import { Integrations } from "@sentry/tracing";
 
-Sentry.init({
-  dsn: process.env['SENTRY_DSN'],
-  integrations: [
-    new VueIntegration({
-      // Vue: Vue, // BUG:  Vue 3 entegration failed :(
-      tracing: true,
-      logErrors: true,
-    }),
-    new Integrations.BrowserTracing(),
-  ],
+// Sentry.init({
+//   dsn: process.env['SENTRY_DSN'],
+//   integrations: [
+//     new VueIntegration({
+//       // Vue: Vue, // BUG:  Vue 3 entegration failed :(
+//       tracing: true,
+//       logErrors: true,
+//     }),
+//     new Integrations.BrowserTracing(),
+//   ],
 
-  // We recommend adjusting this value in production, or using tracesSampler
-  // for finer control
-  tracesSampleRate: 1.0,
-});
+//   // We recommend adjusting this value in production, or using tracesSampler
+//   // for finer control
+//   tracesSampleRate: 1.0,
+// });
 
 createApp(App)
   .use(store)
